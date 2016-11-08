8c001244ea6e2408bd650623d4564de595bd0545
Selim Nowicki <selimnowicki@Dr-Evil-MBP.fritz.box>
2016-11-08 23:02:57 +0100

    ex21

--- lpthw/ex21.py
+++ lpthw/ex21.py
@@ -17,10 +17,18 @@ def divide(a, b):
 
 print  "Let's do some math with just functions!"
 
+'''
 age = add(30 ,5)
 height = substract(78, 4)
 weight = multiply(90, 2)
 iq = divide(100, 2)
+'''
+
+age = add(1, 98)
+height = substract(718.21, 41)
+weight = multiply(90324.11, 2325.44)
+iq = divide(1000.0, 23332)
+
 
 print "Age: %d, Height: %d, Weight: %d, IQ: %d" % (age, height, weight, iq)
 
@@ -31,3 +39,5 @@ print "Here is a puzzle."
 what = add(age, substract(height, multiply(weight, divide(iq, 2))))
 
 print "That becomes: ", what, "Can you do it by hand?"
+
+# (30+5)+((78-4)-(((100/2)/2)*(90*2))) = ???
